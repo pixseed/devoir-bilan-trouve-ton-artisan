214e083a2089d8ca796638072e5cb9be0fc046ef
CedricKernec <cedrickernec.ck@gmail.com>
2026-04-28 16:15:21 +0200

    refactor(ui): mise à jour du footer en utilisant le nouveau système de layout

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -7,33 +7,31 @@ import SocialBar from "../ui/SocialBar";
 export default function Footer() {
   return (
     <footer className="footer">
-      <div className="layout">
-        <div className="container footer__main">
-          {/* LOGO */}
-          <Link to="/" className="footer__logo">
-            <img src={logo} alt="Logo Trouve ton Artisan" />
-          </Link>
+      <div className="container footer__main">
+        {/* LOGO */}
+        <Link to="/" className="footer__logo">
+          <img src={logo} alt="Logo Trouve ton Artisan" />
+        </Link>
 
-          {/* ADRESS */}
-          <address className="footer__address">
-            <strong>Lyon</strong>
-            <div>
-                <p>101 cours Charlemagne</p>
-                <p>CS 20033</p>
-                <p>69269 LYON CEDEX 02</p>
-                <p>France</p>
-            </div>
-          </address>
+        {/* ADRESS */}
+        <address className="footer__address">
+          <strong>Lyon</strong>
+          <div>
+            <p>101 cours Charlemagne</p>
+            <p>CS 20033</p>
+            <p>69269 LYON CEDEX 02</p>
+            <p>France</p>
+          </div>
+        </address>
 
-          {/* NAV */}
-          <nav className="footer__nav">
-            <a href="#">Mentions légales</a>
-            <a href="#">Données personnelles</a>
-            <a href="#">Accessibilité</a>
-            <a href="#">Cookies</a>
-            <a href="#">Contact</a>
-          </nav>
-        </div>
+        {/* NAV */}
+        <nav className="footer__nav">
+          <a href="#">Mentions légales</a>
+          <a href="#">Données personnelles</a>
+          <a href="#">Accessibilité</a>
+          <a href="#">Cookies</a>
+          <a href="#">Contact</a>
+        </nav>
       </div>
 
       {/* SOCIAL MEDIA */}
